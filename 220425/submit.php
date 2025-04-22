62891d40c2ff034efa8b3e666df2678552890d8e
DxT-xDaX <axelyahirmrdez@gmail.com>
2025-04-22 16:20:33 -0600

    Add files via upload

---
+++ 220425/submit.php
@@ -0,0 +1,30 @@
+<?php
+$nombreUsuario=$_GET['nombreUsuario'];
+$carreraUsuario=$_GET['usuarioCarrera'];
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+    <div>
+    <h1>Hola</h1>
+    </div>
+    </section>
+</body>
+</html>
+
+/*$nombre='Aaron';
+$dato1=10;
+$dato2=5;
+$suma=$dato1+$dato2;
+echo "<p> Datp 1=".$dato1."</p> <br>";
+echo "<p> Datp 2=".$dato2."</p> <br>";
+echo "<h1> El resultqado es:".$suma."</p> <br>";
+echo $nombre;
+*/
+?>
